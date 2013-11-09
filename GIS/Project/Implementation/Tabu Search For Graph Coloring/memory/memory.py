a8c2569dc6533b838890c3927c6af69edfcdffac
Jakub Turek <jkbturek@gmail.com>
2013-11-09 13:15:51 +0100

    Introduced short- and long-term memory

---
+++ GIS/Project/Implementation/Tabu Search For Graph Coloring/memory/memory.py
@@ -0,0 +1,16 @@
+class Memory:
+    def __init__(self, short_term_memory_size):
+        self.memory = []
+        self.short_term_memory_size = short_term_memory_size
+
+    def add_to_memory(self, node, color):
+        self.memory.append((node, color))
+
+    def clear_memory(self):
+        self.memory = []
+
+    def is_in_long_term_memory(self, node, color):
+        return (node, color) in self.memory
+
+    def is_in_short_term_memory(self, node, color):
+        return (node, color) in self.memory[-self.short_term_memory_size:]
---
+++ GIS/Project/Implementation/Tabu Search For Graph Coloring/test/memory_tests.py
@@ -0,0 +1,75 @@
+import unittest
+from graph.node import Node
+from memory.memory import Memory
+
+
+class MemoryTests(unittest.TestCase):
+    memory = Memory(3)
+
+    def test_is_in_short_term_memory_method(self):
+        self.memory.clear_memory()
+
+        n1 = Node(1)
+        n2 = Node(2)
+        n3 = Node(3)
+        n4 = Node(4)
+
+        self.memory.add_to_memory(n1, 2)
+        n1_2_in_memory_first = self.memory.is_in_short_term_memory(n1, 2)
+        self.memory.add_to_memory(n2, 3)
+        n1_2_in_memory_second = self.memory.is_in_short_term_memory(n1, 2)
+        n2_3_in_memory_first = self.memory.is_in_short_term_memory(n2, 3)
+        self.memory.add_to_memory(n3, 4)
+        n1_2_in_memory_third = self.memory.is_in_short_term_memory(n1, 2)
+        n2_3_in_memory_second = self.memory.is_in_short_term_memory(n2, 3)
+        n3_4_in_memory_first = self.memory.is_in_short_term_memory(n3, 4)
+        self.memory.add_to_memory(n4, 1)
+        n1_2_in_memory_fourth = self.memory.is_in_short_term_memory(n1, 2)
+        n2_3_in_memory_third = self.memory.is_in_short_term_memory(n2, 3)
+        n3_4_in_memory_second = self.memory.is_in_short_term_memory(n3, 4)
+        n4_1_in_memory_first = self.memory.is_in_short_term_memory(n4, 1)
+
+        self.assertTrue(n1_2_in_memory_first)
+        self.assertTrue(n1_2_in_memory_second)
+        self.assertTrue(n2_3_in_memory_first)
+        self.assertTrue(n1_2_in_memory_third)
+        self.assertTrue(n2_3_in_memory_second)
+        self.assertTrue(n3_4_in_memory_first)
+        self.assertFalse(n1_2_in_memory_fourth)
+        self.assertTrue(n2_3_in_memory_third)
+        self.assertTrue(n3_4_in_memory_second)
+        self.assertTrue(n4_1_in_memory_first)
+
+    def test_is_in_long_term_memory_method(self):
+        self.memory.clear_memory()
+
+        n1 = Node(1)
+        n2 = Node(2)
+        n3 = Node(3)
+        n4 = Node(4)
+
+        self.memory.add_to_memory(n1, 2)
+        n1_2_in_memory_first = self.memory.is_in_long_term_memory(n1, 2)
+        self.memory.add_to_memory(n2, 3)
+        n1_2_in_memory_second = self.memory.is_in_long_term_memory(n1, 2)
+        n2_3_in_memory_first = self.memory.is_in_long_term_memory(n2, 3)
+        self.memory.add_to_memory(n3, 4)
+        n1_2_in_memory_third = self.memory.is_in_long_term_memory(n1, 2)
+        n2_3_in_memory_second = self.memory.is_in_long_term_memory(n2, 3)
+        n3_4_in_memory_first = self.memory.is_in_long_term_memory(n3, 4)
+        self.memory.add_to_memory(n4, 1)
+        n1_2_in_memory_fourth = self.memory.is_in_long_term_memory(n1, 2)
+        n2_3_in_memory_third = self.memory.is_in_long_term_memory(n2, 3)
+        n3_4_in_memory_second = self.memory.is_in_long_term_memory(n3, 4)
+        n4_1_in_memory_first = self.memory.is_in_long_term_memory(n4, 1)
+
+        self.assertTrue(n1_2_in_memory_first)
+        self.assertTrue(n1_2_in_memory_second)
+        self.assertTrue(n2_3_in_memory_first)
+        self.assertTrue(n1_2_in_memory_third)
+        self.assertTrue(n2_3_in_memory_second)
+        self.assertTrue(n3_4_in_memory_first)
+        self.assertTrue(n1_2_in_memory_fourth)
+        self.assertTrue(n2_3_in_memory_third)
+        self.assertTrue(n3_4_in_memory_second)
+        self.assertTrue(n4_1_in_memory_first)
