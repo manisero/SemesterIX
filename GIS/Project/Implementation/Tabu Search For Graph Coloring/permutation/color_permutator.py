24ea44d5a48adf61f6d1391ca4a27b8b88f4c66b
Jakub Turek <jkbturek@gmail.com>
2013-11-10 18:39:21 +0100

    Introduced banned transitions in color permutator

--- GIS/Project/Implementation/Tabu Search For Graph Coloring/permutation/color_permutator.py
+++ GIS/Project/Implementation/Tabu Search For Graph Coloring/permutation/color_permutator.py
@@ -7,18 +7,21 @@ class ColorPermutator:
         self.inspected_nodes = set()
         self.permutations = []
 
-    def permutate(self, root_node, color_set):
+    def permutate(self, root_node, color_set, banned_transitions=None):
         self.inspected_nodes = set()
         self.permutations = []
-        self.find_permutations(root_node, color_set)
+        self.find_permutations(root_node, color_set, banned_transitions)
 
         return self.permutations
 
-    def find_permutations(self, node, color_set):
+    def find_permutations(self, node, color_set, banned_transitions):
         self.inspected_nodes.add(node)
 
         for color in color_set:
             if node.color == color:
+                continue
+
+            if banned_transitions is not None and (node.node_id, color) in banned_transitions:
                 continue
 
             cloned_node = self.graph_cloner.clone(node)
@@ -28,4 +31,4 @@ class ColorPermutator:
 
         for child_node in node.edges:
             if child_node not in self.inspected_nodes:
-                self.find_permutations(child_node, color_set)
+                self.find_permutations(child_node, color_set, banned_transitions)
--- GIS/Project/Implementation/Tabu Search For Graph Coloring/test/color_permutator_tests.py
+++ GIS/Project/Implementation/Tabu Search For Graph Coloring/test/color_permutator_tests.py
@@ -41,3 +41,31 @@ class ColorPermutatorTests(unittest.TestCase):
         self.assertEqual(2, permutations[5].color)
         self.assertEqual(1, permutations[5][0].color)
         self.assertEqual(2, permutations[5][0][0].color)
+
+    def test_permutate_method_with_banned_transitions(self):
+        n1 = Node(1, 18)
+        n2 = Node(2, 23)
+        n3 = Node(3, 51)
+        n1.add_edges([n2])
+        n2.add_edges([n3])
+        n3.add_edges([n1])
+
+        permutations = self.color_permutator.permutate(n1, [1, 2, 3], [(23, 1), (51, 1)])
+
+        self.assertEqual(4, len(permutations))
+
+        self.assertEqual(2, permutations[0].color)
+        self.assertEqual(2, permutations[0][0].color)
+        self.assertEqual(3, permutations[0][0][0].color)
+
+        self.assertEqual(3, permutations[1].color)
+        self.assertEqual(2, permutations[1][0].color)
+        self.assertEqual(3, permutations[1][0][0].color)
+
+        self.assertEqual(3, permutations[2].color)
+        self.assertEqual(3, permutations[2][0].color)
+        self.assertEqual(1, permutations[2][0][0].color)
+
+        self.assertEqual(2, permutations[3].color)
+        self.assertEqual(1, permutations[3][0].color)
+        self.assertEqual(2, permutations[3][0][0].color)
