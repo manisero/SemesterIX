e46bfddb7195195da3713e35968a0fede8dfd9d8
Jakub Turek <jkbturek@gmail.com>
2013-11-12 22:16:03 +0100

    Changed amount of color classes is now included in input as data

--- GIS/Project/Implementation/Tabu Search For Graph Coloring/input/input_reader.py
+++ GIS/Project/Implementation/Tabu Search For Graph Coloring/input/input_reader.py
@@ -1,30 +1,38 @@
+import random
 from graph.node import Node
 
 
 class InputReader:
-    ColorId = 1
-
     def __init__(self):
         self.id_to_note_mapping = {}
         self.edge_ids = []
+        self.color_classes_amount = None
 
     def input_graph(self, file_name):
         self.reset_input()
 
         with open(file_name, 'r') as graph_file:
             for line in graph_file:
+                if self.color_classes_amount is None:
+                    self.color_classes_amount = int(line.strip())
+
+                    if self.color_classes_amount is None or self.color_classes_amount <= 0:
+                        raise Exception('Invalid input format!')
+
+                    continue
+
                 edge = line.strip().split(',')
 
                 if len(edge) != 2:
                     raise Exception('Invalid input format!')
 
                 if edge[0] not in self.id_to_note_mapping:
-                    self.id_to_note_mapping[edge[0]] = Node(color=InputReader.ColorId, node_id=edge[0])
-                    InputReader.ColorId += 1
+                    self.id_to_note_mapping[edge[0]] = Node(color=random.choice(range(0, self.color_classes_amount)),
+                                                            node_id=edge[0])
 
                 if edge[1] not in self.id_to_note_mapping:
-                    self.id_to_note_mapping[edge[1]] = Node(color=InputReader.ColorId, node_id=edge[1])
-                    InputReader.ColorId += 1
+                    self.id_to_note_mapping[edge[1]] = Node(color=random.choice(range(0, self.color_classes_amount)),
+                                                            node_id=edge[1])
 
                 self.edge_ids.append(edge)
 
