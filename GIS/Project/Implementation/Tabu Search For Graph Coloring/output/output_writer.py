b48777f82655647f3f9399649c17d86b796ec252
Jakub Turek <jkbturek@gmail.com>
2013-11-12 23:17:24 +0100

    Introduced main method

--- GIS/Project/Implementation/Tabu Search For Graph Coloring/output/output_writer.py
+++ GIS/Project/Implementation/Tabu Search For Graph Coloring/output/output_writer.py
@@ -4,6 +4,13 @@ class OutputWriter:
 
     @staticmethod
     def write_graph(root_node, file_name):
-        with open(file_name, 'w') as output_file:
-            for node in root_node.iterator():
-                output_file.write(str(node.node_id) + ' - ' + str(node.color) + '\n')
+        if isinstance(file_name, str):
+            with open(file_name, 'w') as output_file:
+                OutputWriter.write_to_output_file(root_node, output_file)
+        else:
+            OutputWriter.write_to_output_file(root_node, file_name)
+
+    @staticmethod
+    def write_to_output_file(root_node, output_file):
+        for node in root_node.iterator():
+            output_file.write(str(node.node_id) + ' - ' + str(node.color) + '\n')
