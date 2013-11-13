8092d6227e23cecf6b36c068d1f029d018848ee1
Jakub Turek <jkbturek@gmail.com>
2013-11-13 20:10:15 +0100

    Introduced DSA algorithm utilities

---
+++ PTKB/DSA/Implementation/dsa/dsa_utilities.py
@@ -0,0 +1,10 @@
+from random import randrange
+
+
+class DSAUtilities:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def generate_random_p_bit_length():
+        return randrange(512, 1068, 64)
