17139fd14b33095a50db1fa870e4243ae4f6d393
manisero <michalaniserowicz@gmail.com>
2013-12-22 12:25:52 +0100

    Removed unused entities

--- MED/Project/Implementation/GRM/GRM.Logic/DataSetProcessing/Entities/ConcreteItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GRM.Logic.DataSetProcessing.Entities
-{
-    public struct ConcreteItem
-    {
-        public Item Item { get; set; }
-
-        public int TransactionID { get; set; }
-    }
-}
--- MED/Project/Implementation/GRM/GRM.Logic/DataSetProcessing/Entities/DataSet.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace GRM.Logic.DataSetProcessing.Entities
-{
-    public class DataSet
-    {
-        public IEnumerable<Transaction> Transactions { get; set; }
-    }
-}
