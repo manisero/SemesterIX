250e4b09d889b74201dae5f7e18966ada5973691
manisero <michalaniserowicz@gmail.com>
2013-12-22 13:05:03 +0100

    Added sample data file

--- MED/Project/Implementation/GRM/GRM.Presentation/Program.cs
+++ MED/Project/Implementation/GRM/GRM.Presentation/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace GRM.Presentation
+﻿using System;
+using System.IO;
+using GRM.Logic.DataSetProcessing._Impl;
+
+namespace GRM.Presentation
 {
     class Program
     {
         static void Main(string[] args)
         {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage:");
+
+                var applicationPath = Environment.GetCommandLineArgs()[0];
+                Console.WriteLine("{0} <data file path>", Path.GetFileName(applicationPath));
+                return;
+            }
+
+            var stream = new FileStream(args[0], FileMode.Open, FileAccess.Read, FileShare.Read);
+
+            var representation = new DataSetRepresentationBuilder(new TransactionProcessor()).Build(stream);
         }
     }
 }
