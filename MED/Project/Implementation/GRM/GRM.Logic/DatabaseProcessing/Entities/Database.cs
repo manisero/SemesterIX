c57d814bcc776513167f3f673af32d5dd6fb6cad
manisero <michalaniserowicz@gmail.com>
2013-12-22 10:26:25 +0100

    Started implementing data set reading

---
+++ MED/Project/Implementation/GRM/GRM.Logic/DatabaseProcessing/Entities/Database.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace GRM.Logic.DatabaseProcessing.Entities
+{
+    public class DataSet
+    {
+        public IEnumerable<Transaction> Transactions { get; set; }
+    }
+}
---
+++ MED/Project/Implementation/GRM/GRM.Logic/DatabaseProcessing/Entities/Transaction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace GRM.Logic.DatabaseProcessing.Entities
+{
+    public class Transaction
+    {
+        public int TransactionID { get; set; }
+
+        public IEnumerable<string[]> Items { get; set; }
+    }
+}
