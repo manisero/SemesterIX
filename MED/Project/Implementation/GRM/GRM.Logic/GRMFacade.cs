27650bc58250a526c3401685133bdffba349c0a2
manisero <michalaniserowicz@gmail.com>
2013-12-22 20:46:39 +0100

    Created GRMFacade

---
+++ MED/Project/Implementation/GRM/GRM.Logic/GRMFacade.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using GRM.Logic.DataSetProcessing._Impl;
+
+namespace GRM.Logic
+{
+    public class GRMFacade
+    {
+        public void ExecuteGRM(string dataFilePath, int minimumSupport)
+        {
+            var stream = new FileStream(dataFilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+
+            var representation = new DataSetRepresentationBuilder(new TransactionProcessor()).Build(stream);
+
+            var frequentItems = new FrequentItemsSelector().SelectFrequentItems(representation.ItemInfos.Values, minimumSupport);
+        }
+    }
+}
--- MED/Project/Implementation/GRM/GRM.Presentation/Program.cs
+++ MED/Project/Implementation/GRM/GRM.Presentation/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.IO;
-using GRM.Logic.DataSetProcessing._Impl;
+using GRM.Logic;
 
 namespace GRM.Presentation
 {
@@ -8,18 +8,18 @@ namespace GRM.Presentation
     {
         static void Main(string[] args)
         {
-            if (args.Length != 1)
+            int minimumSupport;
+
+            if (args.Length != 2 || !int.TryParse(args[1], out minimumSupport))
             {
                 Console.WriteLine("Usage:");
 
                 var applicationPath = Environment.GetCommandLineArgs()[0];
-                Console.WriteLine("{0} <data file path>", Path.GetFileName(applicationPath));
+                Console.WriteLine("{0} <data file path> <minimum support [integer]>", Path.GetFileName(applicationPath));
                 return;
             }
 
-            var stream = new FileStream(args[0], FileMode.Open, FileAccess.Read, FileShare.Read);
-
-            var representation = new DataSetRepresentationBuilder(new TransactionProcessor()).Build(stream);
+            new GRMFacade().ExecuteGRM(args[0], minimumSupport);
         }
     }
 }
