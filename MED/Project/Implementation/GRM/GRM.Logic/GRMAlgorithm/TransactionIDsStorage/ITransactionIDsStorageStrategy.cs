1dd31773e4aa2b26026efbd35fc67c2b805c0214
manisero <michalaniserowicz@gmail.com>
2013-12-27 12:16:22 +0100

    Filled ITransactionIDsStorageStrategy interface with methods

--- MED/Project/Implementation/GRM/GRM.Logic/GRMAlgorithm/TransactionIDsStorage/ITransactionIDsStorageStrategy.cs
+++ MED/Project/Implementation/GRM/GRM.Logic/GRMAlgorithm/TransactionIDsStorage/ITransactionIDsStorageStrategy.cs
@@ -1,7 +1,15 @@
-﻿namespace GRM.Logic.GRMAlgorithm.TransactionIDsStorage
+﻿using System.Collections.Generic;
+
+namespace GRM.Logic.GRMAlgorithm.TransactionIDsStorage
 {
     public interface ITransactionIDsStorageStrategy
     {
-        
+        IList<int> GetTreeRootTransactionIDs(IList<int> allTransactionIds);
+
+        int GetTreeRootSupport(int allTransactionIdsCount);
+
+        IList<int> GetChildTransactionIDs(IList<int> parentTransactionIds, IEnumerable<int> parentSiblingTransactionIds);
+
+        int GetChildSupport(int parentSupport, IList<int> childTransactionIds);
     }
 }
--- MED/Project/Implementation/GRM/GRM.Logic/GRMAlgorithm/TransactionIDsStorage/StorageStrategies/TIDSetsStorageStrategy.cs
+++ MED/Project/Implementation/GRM/GRM.Logic/GRMAlgorithm/TransactionIDsStorage/StorageStrategies/TIDSetsStorageStrategy.cs
@@ -1,6 +1,27 @@
-﻿namespace GRM.Logic.GRMAlgorithm.TransactionIDsStorage.StorageStrategies
+﻿using System.Collections.Generic;
+
+namespace GRM.Logic.GRMAlgorithm.TransactionIDsStorage.StorageStrategies
 {
     public class TIDSetsStorageStrategy : ITransactionIDsStorageStrategy
     {
+        public IList<int> GetTreeRootTransactionIDs(IList<int> allTransactionIds)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetTreeRootSupport(int allTransactionIdsCount)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<int> GetChildTransactionIDs(IList<int> parentTransactionIds, IEnumerable<int> parentSiblingTransactionIds)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetChildSupport(int parentSupport, IList<int> childTransactionIds)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
