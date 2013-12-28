6ab1676d8ab5cc8728978b0506091b08131aef8b
manisero <michalaniserowicz@gmail.com>
2013-12-28 21:26:47 +0100

    Created GRMFacade tests in PerformanceTests project

---
+++ MED/Project/Implementation/GRM/GRM.Logic.PerformanceTests/GRMFacade/CarData_MinSup10.cs
@@ -0,0 +1,22 @@
+﻿using GRM.Logic.GRMAlgorithm.Entities;
+using GRM.Logic.UnitTests.GRMFacade;
+
+namespace GRM.Logic.PerformanceTests.GRMFacade
+{
+    public class CarData_MinSup10 : GRMFacadeTestsBase
+    {
+        protected override string DataSet
+        {
+            get { return Resources.Resources.car_100k; }
+        }
+
+        protected override int MinimumSupport
+        {
+            get { return 10; }
+        }
+
+        protected override void AssertResult(GRMResult result)
+        {
+        }
+    }
+}
