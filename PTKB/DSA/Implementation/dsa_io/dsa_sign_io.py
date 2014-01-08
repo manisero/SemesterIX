2f1f6ceac8d00aa4c8963393f073add1deeb1292
Jakub Turek <jkbturek@gmail.com>
2014-01-08 21:37:50 +0100

    Implemented veryfing DSA sign from file

--- PTKB/DSA/Implementation/dsa_io/dsa_sign_io.py
+++ PTKB/DSA/Implementation/dsa_io/dsa_sign_io.py
@@ -1,3 +1,6 @@
+import re
+
+
 class DSASignIO:
     def __init__(self):
         pass
@@ -11,4 +14,21 @@ class DSASignIO:
 
     @staticmethod
     def verify_sign_from_file(dsa_key, filename):
-        pass
+        with open(filename, 'r') as import_file:
+            import_file_contents = import_file.readlines()
+
+            if len(import_file_contents) <= 1:
+                raise IOError('Invalid file contents')
+
+            match = re.search('\((\d+),(\d+)\)', import_file_contents[0].strip())
+
+            if match is None or len(match.groups()) != 2:
+                raise IOError('Invalid file contents')
+
+            try:
+                r, s = long(match.group(1)), long(match.group(2))
+                message = '\n'.join([x.strip() for x in import_file_contents[1:]])
+            except:
+                raise IOError('Invalid file contents')
+
+            return dsa_key.verify(message, r, s)
--- PTKB/DSA/Implementation/tests/dsa_sign_io_tests.py
+++ PTKB/DSA/Implementation/tests/dsa_sign_io_tests.py
@@ -1,6 +1,6 @@
 from tempfile import NamedTemporaryFile
 import unittest
-from mock import MagicMock
+from mock import Mock
 from dsa.dsa_key import DSAKey
 from dsa_io.dsa_sign_io import DSASignIO
 
@@ -10,7 +10,7 @@ class DSASignIOTests(unittest.TestCase):
         with NamedTemporaryFile() as sign_file:
             message = 'This\nis\njust\na\ntest\nmessage'
             dsa_key = DSAKey()
-            dsa_key.sign = MagicMock(return_value=(1, 2))
+            dsa_key.sign = Mock(return_value=(1, 2))
             DSASignIO.export_sign_to_file(dsa_key, message, sign_file.name)
             sign_file.seek(0)
 
@@ -26,4 +26,24 @@ class DSASignIOTests(unittest.TestCase):
             self.assertEqual('message', file_contents[6].strip())
 
     def test_verify_sign_from_file_method(self):
-        self.fail()
+        with NamedTemporaryFile() as sign_file:
+            message = 'Some message'
+            sign = 4, 6
+            dsa_key = DSAKey()
+            dsa_key.verify = Mock(side_effect=lambda m, r, s: m == message and r == sign[0] and s == sign[1])
+            sign_file.write('({0},{1})\n{2}\n'.format(sign[0], sign[1], message))
+            sign_file.seek(0)
+
+            verify_result = DSASignIO.verify_sign_from_file(dsa_key, sign_file.name)
+
+            self.assertTrue(verify_result)
+
+        with NamedTemporaryFile() as sign_file:
+            message = 'Another piece of message'
+            sign = 1, 2
+            sign_file.write('({0},{1})\n{2}\n'.format(sign[0] + 1, sign[1] - 1, message))
+            sign_file.seek(0)
+
+            verify_result = DSASignIO.verify_sign_from_file(dsa_key, sign_file.name)
+
+            self.assertFalse(verify_result)
