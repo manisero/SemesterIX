9fac50f8c0f06bc86cc06c41880268272415a9e0
manisero <michalaniserowicz@gmail.com>
2014-01-12 23:31:15 +0100

    Started implementing InvertedListsDecisionGeneratorCollector

---
+++ MED/Project/Implementation/GRM/GRM.Logic/GRMAlgorithm/DecisionGeneratorsCollecting/Collectors/InvertedListsDecisionGeneratorsCollector.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using GRM.Logic.DataSetProcessing.Entities;
+using GRM.Logic.GRMAlgorithm.Entities;
+
+namespace GRM.Logic.GRMAlgorithm.DecisionGeneratorsCollecting.Collectors
+{
+    public class InvertedListsDecisionGeneratorsCollector : DecisionGeneratorsCollectorBase
+    {
+        private class InvertedList : Dictionary<ItemID, IList<Generator>>
+        {
+        }
+
+        private readonly IDictionary<int, InvertedList> _decisionInvertedLists = new Dictionary<int, InvertedList>();
+
+        protected override void AppendGenerators(int decisionId, IList<Generator> generators)
+        {
+            if (!_decisionInvertedLists.ContainsKey(decisionId))
+            {
+                _decisionInvertedLists.Add(decisionId, new InvertedList());
+            }
+
+            var invertedList = _decisionInvertedLists[decisionId];
+
+            foreach (var generator in generators)
+            {
+                RemoveSupergenerators(generator, invertedList);
+            }
+
+            foreach (var generator in generators)
+            {
+                AppendGenerator(generator, invertedList);
+            }
+        }
+
+        private void RemoveSupergenerators(Generator subgenerator, InvertedList invertedList)
+        {
+            // TODO: Implement
+        }
+
+        private void AppendGenerator(Generator generator, InvertedList invertedList)
+        {
+            foreach (var item in generator)
+            {
+                if (!invertedList.ContainsKey(item))
+                {
+                    invertedList.Add(item, new List<Generator> { generator });
+                }
+                else
+                {
+                    invertedList[item].Add(generator);
+                }
+            }
+        }
+
+        public override IDictionary<int, IList<Generator>> GetDecisionsGenerators()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
