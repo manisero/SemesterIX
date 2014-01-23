6aed46647060e2dd477bbcea47d4eb6e62e19f65
manisero <michalaniserowicz@gmail.com>
2014-01-23 23:15:24 +0100

    Hidden BruteForce strategy for supergenerators handling

--- MED/Project/Implementation/GRM/GRM.Logic/GRMAlgorithm/DecisionGeneratorsCollecting/DecisionSupergeneratorsHandlingStrategyType.cs
+++ MED/Project/Implementation/GRM/GRM.Logic/GRMAlgorithm/DecisionGeneratorsCollecting/DecisionSupergeneratorsHandlingStrategyType.cs
@@ -3,7 +3,7 @@
     public enum DecisionSupergeneratorsHandlingStrategyType
     {
         InvertedLists,
-        BruteForce,
-        BruteForceLINQ
+        BruteForceLINQ,
+        BruteForce
     }
 }
