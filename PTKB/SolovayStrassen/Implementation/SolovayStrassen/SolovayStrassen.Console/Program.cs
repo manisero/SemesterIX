3f902bc2d73c942a6870b5b30b4d641a5217f7b4
manisero <michalaniserowicz@gmail.com>
2014-01-24 21:31:13 +0100

    Started comparing two algorithms

--- PTKB/SolovayStrassen/Implementation/SolovayStrassen/SolovayStrassen.Console/Program.cs
+++ PTKB/SolovayStrassen/Implementation/SolovayStrassen/SolovayStrassen.Console/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace SolovayStrassen.Console
+﻿using System.Numerics;
+using SolovayStrassen.Logic;
+
+namespace SolovayStrassen.Console
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var codeProjectResult = new Logic.CodeProject.BigInteger(16769023L).SolovayStrassenTest(100);
+
+            var myResult = SolovayStrassenAlgorithm.Execute(new BigInteger(16769023L), 100);
         }
     }
 }
