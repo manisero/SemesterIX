b73122787c8be09544ab77ba30117f51ce2a93e9
manisero <michalaniserowicz@gmail.com>
2014-01-24 20:52:16 +0100

    Formatted code

--- PTKB/SolovayStrassen/Implementation/SolovayStrassen/SolovayStrassen.Logic/JacobiAlgorithm.cs
+++ PTKB/SolovayStrassen/Implementation/SolovayStrassen/SolovayStrassen.Logic/JacobiAlgorithm.cs
@@ -9,21 +9,40 @@ namespace SolovayStrassen.Logic
         {
             // Jacobi defined only for odd integers
             if ((b.data[0] & 0x1) == 0)
-                throw (new ArgumentException("Jacobi defined only for odd integers."));
+            {
+                throw (new ArgumentException("Jacobi is defined only for odd numbers."));
+            }
 
-            if (a >= b) a %= b;
-            if (a.dataLength == 1 && a.data[0] == 0) return 0;  // a == 0
-            if (a.dataLength == 1 && a.data[0] == 1) return 1;  // a == 1
+            if (a >= b)
+            {
+                a %= b;
+            }
+
+            if (a.dataLength == 1 && a.data[0] == 0)
+            {
+                return 0; // a == 0
+            }
+
+            if (a.dataLength == 1 && a.data[0] == 1)
+            {
+                return 1; // a == 1
+            }
 
             if (a < 0)
             {
-                if ((((b - 1).data[0]) & 0x2) == 0)       //if( (((b-1) >> 1).data[0] & 0x1) == 0)
+                //if( (((b-1) >> 1).data[0] & 0x1) == 0)
+                if ((((b - 1).data[0]) & 0x2) == 0)
+                {
                     return Jacobi(-a, b);
+                }
                 else
+                {
                     return -Jacobi(-a, b);
+                }
             }
 
             int e = 0;
+
             for (int index = 0; index < a.dataLength; index++)
             {
                 uint mask = 0x01;
@@ -32,27 +51,37 @@ namespace SolovayStrassen.Logic
                 {
                     if ((a.data[index] & mask) != 0)
                     {
-                        index = a.dataLength;      // to break the outer loop
+                        // to break the outer loop
+                        index = a.dataLength;
                         break;
                     }
+
                     mask <<= 1;
                     e++;
                 }
             }
 
             BigInteger a1 = a >> e;
-
             int s = 1;
+
             if ((e & 0x1) != 0 && ((b.data[0] & 0x7) == 3 || (b.data[0] & 0x7) == 5))
+            {
                 s = -1;
+            }
 
             if ((b.data[0] & 0x3) == 3 && (a1.data[0] & 0x3) == 3)
+            {
                 s = -s;
+            }
 
             if (a1.dataLength == 1 && a1.data[0] == 1)
+            {
                 return s;
+            }
             else
+            {
                 return (s * Jacobi(b % a1, a1));
+            }
         }
     }
 }
