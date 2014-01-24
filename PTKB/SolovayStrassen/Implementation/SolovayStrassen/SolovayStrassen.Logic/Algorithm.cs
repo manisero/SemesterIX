090b73b2b2a600480dc79026f5e1552b1fba94e1
manisero <michalaniserowicz@gmail.com>
2014-01-24 20:40:22 +0100

    Implemented GCD test

--- PTKB/SolovayStrassen/Implementation/SolovayStrassen/SolovayStrassen.Logic/Algorithm.cs
+++ PTKB/SolovayStrassen/Implementation/SolovayStrassen/SolovayStrassen.Logic/Algorithm.cs
@@ -5,47 +5,47 @@ namespace SolovayStrassen.Logic
 {
     public class Algorithm
     {
-        public bool SolovayStrassenTest(BigInteger thisVal, int confidence)
+        public bool SolovayStrassenTest(BigInteger number, int confidence)
         {
             // Check for small numbers
-            if (thisVal.IsZero || thisVal.IsOne)
+            if (number.IsZero || number.IsOne)
             {
                 return false;
             }
 
-            if (thisVal == 2 || thisVal == 3)
+            if (number == 2 || number == 3)
             {
                 return true;
             }
 
             // Check for even numbers
-            if (thisVal.IsEven)
+            if (number.IsEven)
             {
                 return false;
             }
 
             // Perform primality test
-            var numberBytesCount = thisVal.ToByteArray().Length;
+            var numberBytesCount = number.ToByteArray().Length;
             var random = new Random();
 
             // TODO:
-            BigInteger p_sub1 = thisVal - 1;
+            BigInteger p_sub1 = number - 1;
             BigInteger p_sub1_shift = p_sub1 >> 1;
 
             for (int round = 0; round < confidence; round++)
             {
-                BigInteger a = GenerateA(thisVal, numberBytesCount, random);
+                var a = GenerateA(number, numberBytesCount, random);
 
-                // check whether a factor exists
-                BigInteger gcdTest = a.gcd(thisVal);
+                // Check whether a factor exists
+                var gcd = BigInteger.GreatestCommonDivisor(number, a);
 
-                if (gcdTest.dataLength == 1 && gcdTest.data[0] != 1)
+                if (!gcd.IsOne)
                 {
                     return false;
                 }
 
                 // calculate a^((p-1)/2) mod p
-                BigInteger expResult = a.modPow(p_sub1_shift, thisVal);
+                BigInteger expResult = a.modPow(p_sub1_shift, number);
 
                 if (expResult == p_sub1)
                 {
@@ -53,7 +53,7 @@ namespace SolovayStrassen.Logic
                 }
 
                 // calculate Jacobi symbol
-                BigInteger jacob = Jacobi(a, thisVal);
+                BigInteger jacob = Jacobi(a, number);
 
                 //Console.WriteLine("a = " + a.ToString(10) + " b = " + thisVal.ToString(10));
                 //Console.WriteLine("expResult = " + expResult.ToString(10) + " Jacob = " + jacob.ToString(10));
