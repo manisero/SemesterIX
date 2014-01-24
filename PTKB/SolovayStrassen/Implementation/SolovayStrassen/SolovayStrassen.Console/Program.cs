5622b35295db8a699c0c6ea97c64e3efb9b972de
manisero <michalaniserowicz@gmail.com>
2014-01-24 22:39:28 +0100

    Started code cleanup

--- PTKB/SolovayStrassen/Implementation/SolovayStrassen/SolovayStrassen.Console/Program.cs
+++ PTKB/SolovayStrassen/Implementation/SolovayStrassen/SolovayStrassen.Console/Program.cs
@@ -8,22 +8,15 @@ namespace SolovayStrassen.Console
     {
         static void Main(string[] args)
         {
-            long result1 = 0;
-            long result2 = 0;
+            var result = 0L;
 
             for (int i = 0; i < 1000; i++)
             {
-                var stopwatch1 = new Stopwatch();
-                stopwatch1.Start();
-                var codeProjectResult = new Logic.CodeProject.BigInteger(16769023L).SolovayStrassenTest(100);
-                stopwatch1.Stop();
-                result1 += stopwatch1.ElapsedMilliseconds;
-
                 var stopwatch2 = new Stopwatch();
                 stopwatch2.Start();
-                var myResult = SolovayStrassenAlgorithm.Execute(new BigInteger(16769023L), 100);
+                SolovayStrassenAlgorithm.Execute(new BigInteger(16769023L), 100);
                 stopwatch2.Stop();
-                result2 += stopwatch2.ElapsedMilliseconds;
+                result += stopwatch2.ElapsedMilliseconds;
             }
         }
     }
